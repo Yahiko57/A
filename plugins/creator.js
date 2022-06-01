95e9e94f268b6d9ecadd5d66db7d4916e9174f0b
virakun <93888991+virakun@users.noreply.github.com>
2022-06-01 16:29:35 +0900

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -2,13 +2,13 @@ const PhoneNumber = require('awesome-phonenumber')
 async function handler(m) {
                 let vcard = 'BEGIN:VCARD\n' // metadata of the contact card
                     + 'VERSION:3.0\n' 
-                    + 'N:;Lui;;;'
-                    + 'FN:AhmdLui ルイ\n' // full name
+                    + 'N:;febri;;;'
+                    + 'FN:febri\n' // full name
                     + 'item1.X-ABLabel:Mau masukin bot ke gc Lu chat Gw\n'
                     + 'URL;Web gwejh: doujindesu.xxx\n'
-                    + 'EMAIL;Email Owner: njircoeg397@gmail.com\n'
-                    + 'ORG:Lord Lui ルイ;\n' // the organization of the contact
-                    + 'TEL;type=CELL;type=VOICE;waid=6282146092695:+6282146092695\n' // WhatsApp ID + phone number
+                    + 'EMAIL;Email Owner: febrieka8800@gmail.com\n'
+                    + 'ORG:Lord febri;\n' // the organization of the contact
+                    + 'TEL;type=CELL;type=VOICE;waid=818076446361:+818076446361\n' // WhatsApp ID + phone number
                     + 'END:VCARD'
                 conn.sendMessage(m.chat, { contacts: { displayName: 'Lol Animals', contacts: [{ vcard }] } }, { quoted: m })
 }
