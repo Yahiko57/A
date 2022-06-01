037897812bd764b80e5580639cb99d1b5b48e94c
virakun <93888991+virakun@users.noreply.github.com>
2022-06-01 16:27:23 +0900

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ second: 'numeric'
 
 function pickRandom(list) {
   return list[Math.floor(Math.random() * list.length)]
-}global.owner = ['6282146092695'] // Put your number here
+}global.owner = ['818076446361'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -64,19 +64,19 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.stiker_wait = 'Stiker sedang dibuat'
-global.packname = 'Rika'
+global.packname = 'BOT'
 global.author = '>////<'
 
 
 
 
 
-ig: 'ahmdlui'
-global.wm = 'SoikemMd'
+ig: 'botwa'
+global.wm = 'BOTWA'
 global.watermark = 'wm'
 global.botdate = '╭──────────═┅═─────────\n│◪ ${time}\n┊◪ ${week} ${date}\n╰──────────═┅═─────────'
 global.media = 'https://telegra.ph/file/e001198ced5650ef53d47.jpg'
-global.instagram = 'https://instagram.com/ahmdlui'
+global.instagram = 'https://instagram.com/doi_editz'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&doScale=true&scaleWidth=500&scaleHeight=500&fontsize=100&fillTextType=0&backgroundColor=%23401620&text='
 global.wait = '_*tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
